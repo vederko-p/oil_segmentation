d9471519e93b96cfb0290715dcf66ac38b5b4033
Skibin Maksim <ski6a_23@mail.ru>
2023-03-24 22:36:51 +0300

    image slicing: add image slicing into subframes | add padding preproccessing before slicing

--- notebooks/image_stitching/image_slicing.py
+++ notebooks/image_stitching/image_slicing.py
@@ -32,3 +32,37 @@ def padding_before_sub(img, w_size):
         np.pad(img[:, :, ch], padding_shape)
         for ch in range(img.shape[-1])
     ]).transpose(1, 2, 0)
+
+
+def extract_sub_frames(img, win_h, win_w):
+    """Amount of windows by x example:
+
+    window.shape ~ (win_h, win_w)
+    x stride (dx) ~ win_w
+    y stride (dy) ~ win_h
+
+    (img.shape[-2] - windows.shape[-2]) // dx + 1  =>
+    (img.shape[-2] - win_w) // win_w + 1           =>
+    img.shape[-2] // win_w - 1 + 1                 =>
+    img.shape[-2] // win_w
+    """
+    # calculate new shape:
+    new_shape = (
+            img.shape[:-2]  # amount of rows
+            + (img.shape[-2] // win_h,)  # amount of windows by y
+            + (img.shape[-1] // win_w,)  # amount of windows by x
+            + (win_h, win_w)  # size of window
+    )
+    # calculate new strides:
+    new_strides = (
+            img.strides[:-2]
+            + (img.strides[-2] * win_h,)
+            + (img.strides[-1] * win_w,)
+            + img.strides[-2:]
+    )
+    # rolling window:
+    return np.lib.stride_tricks.as_strided(
+        img,
+        shape=new_shape,
+        strides=new_strides
+    )
