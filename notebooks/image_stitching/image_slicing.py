8b32d2dd4f577cd754243cac7d99e47f41975098
Skibin Maksim <ski6a_23@mail.ru>
2023-03-24 22:36:27 +0300

    image slicing: add image slicing into subframes | add padding preproccessing before slicing

--- notebooks/image_stitching/image_slicing.py
+++ notebooks/image_stitching/image_slicing.py
@@ -9,3 +9,26 @@ def crop_image_by_edges(img):
         mask_y.min(): mask_y.max(),
         mask_x.min(): mask_x.max()
     ]
+
+
+def padding_before_sub(img, w_size):
+    """Padding image to fit whole image in same slices."""
+
+    # Calculate padding:
+    source_shape = img.shape[:-1]
+    target_shape = tuple([
+        ceil(x / w_size) * w_size
+        for x in img.shape[:-1]
+    ])
+    diff_shape = tuple([
+        ts - ss for ss, ts in zip(source_shape, target_shape)
+    ])
+    padding_shape = tuple(
+        [(s // 2, s - (s // 2)) for s in diff_shape]
+    )
+
+    # Padding:
+    return np.stack([
+        np.pad(img[:, :, ch], padding_shape)
+        for ch in range(img.shape[-1])
+    ]).transpose(1, 2, 0)
