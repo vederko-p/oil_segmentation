7b3d5acafc0bcf786cc0166c00545a5f3a16f6c6
Skibin Maksim <ski6a_23@mail.ru>
2023-03-29 16:53:50 +0300

    metrics: fix masks iou | add tensors iou

--- notebooks/experiments/metrics.py
+++ notebooks/experiments/metrics.py
@@ -2,6 +2,12 @@
 import numpy as np
 
 
+def masks_tensors_iou(tens_0, tens_1):
+    intersection = (tens_0 * tens_1).sum(axis=(1, 2, 3))
+    union = ((tens_0 + tens_1) > 0).sum(axis=(1, 2, 3))
+    return intersection / union
+
+
 def masks_iou(mask1: np.array, mask2: np.array):
-    iou = (mask1 == mask2).sum() / ((mask1 + mask2) > 0).sum()
+    iou = (mask1 * mask2).sum() / ((mask1 + mask2) > 0).sum()
     return round(iou, 3)
