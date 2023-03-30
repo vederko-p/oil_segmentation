65aaaa432c31a5284317830d3e8b656a02db9e96
Nick Sidelnikov <nikita04042000@gmail.com>
2023-03-30 17:02:36 +0300

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -8,6 +8,7 @@ import streamlit as st
 from ultralytics import YOLO
 
 from read_video import read_video
+from image_slicing import crop_image_by_edges
 
 
 INPUT_PATH = "../files/input/"
@@ -33,50 +34,54 @@ if input_file is not None:
         status, output = stitcher.stitch(frames)
         if status == 0:
             progress_bar.progress(0.33, text="Processing...")
+            cv2.imwrite(OUTPUT_PATH + input_file.name[:-4] + ".jpg", cv2.cvtColor(output, cv2.COLOR_RGB2BGR))
         else:
-            st.error(f'Error while stitching, status: {status}')
-        cv2.imwrite(OUTPUT_PATH + input_file.name[:-4] + ".jpg", cv2.cvtColor(output, cv2.COLOR_RGB2BGR))
+            st.error(f'Error while stitching, try another video')
 
     if any(ext in input_file.name for ext in [".jpg", ".png"]):
+        status = 0
         info = st.info("Processing picture, please wait")
         progress_bar = st.progress(0.33, text="Processing...")
         output = cv2.imread(INPUT_PATH + input_file.name)
         output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
         cv2.imwrite(OUTPUT_PATH + input_file.name[:-4] + ".jpg", cv2.cvtColor(output, cv2.COLOR_RGB2BGR))
 
-    orig = output.copy()
-    h, w, _ = output.shape
+    if status == 0:
+        output = crop_image_by_edges(output).copy()
+        orig = output.copy()
+        h, w, _ = output.shape
 
-    predictions = model.predict(output)
-        
-    cv2.imwrite(OUTPUT_PATH + input_file.name[:-4] + "_seg.jpg", cv2.cvtColor(output, cv2.COLOR_RGB2BGR))
+        predictions = model.predict(output)
+        output *= orig > 0
+            
+        cv2.imwrite(OUTPUT_PATH + input_file.name[:-4] + "_seg.jpg", cv2.cvtColor(output, cv2.COLOR_RGB2BGR))
 
-    size = cv2.countNonZero(cv2.cvtColor(orig, cv2.COLOR_RGB2GRAY))
+        size = cv2.countNonZero(cv2.cvtColor(orig, cv2.COLOR_RGB2GRAY))
 
-    progress_bar.progress(0.65, text="Calculating the area...")
+        progress_bar.progress(0.65, text="Calculating the area...")
 
-    for object in predictions:
-        if object.masks is not None:
-            mask = np.zeros((h, w))
-            xy = [(object.masks.segments[0] * np.array([[w, h]])).astype(int)]
-            cv2.fillPoly(mask, xy, (255, 255, 255))
-            cv2.imwrite(OUTPUT_PATH + input_file.name[:-4] + "_mask.jpg", mask)
-            mask_size = cv2.countNonZero(mask)
+        for object in predictions:
+            if object.masks is not None:
+                mask = np.zeros((h, w))
+                xy = [(object.masks.segments[0] * np.array([[w, h]])).astype(int)]
+                cv2.fillPoly(mask, xy, (255, 255, 255))
+                cv2.imwrite(OUTPUT_PATH + input_file.name[:-4] + "_mask.jpg", mask)
+                mask_size = cv2.countNonZero(mask)
 
-            spill_size = int(mask_size/size * 100)
+                spill_size = int(mask_size/size * 100)
 
-            info.empty()
-            st.success("Done!")
-            progress_bar.progress(0.99, text="Calculating the area...")
-            st.write(f'Oil spill covers {spill_size} percent of the water surface')
-            progress_bar.empty()
+                info.empty()
+                st.success("Done!")
+                progress_bar.progress(0.99, text="Calculating the area...")
+                st.write(f'Oil spill covers {spill_size} percent of the water surface')
+                progress_bar.empty()
 
-            vis = np.concatenate((orig, output), axis=1)
-            st.image(vis, channels='RGB', caption="TEST")
-        else:
-            st.success("Done!")
-            st.write(f'There is no oil spill')
-            info.empty()
-            progress_bar.empty()
-            st.image(orig, channels='RGB', caption="TEST")
+                vis = np.concatenate((orig, output), axis=1)
+                st.image(vis, channels='RGB', caption="TEST")
+            else:
+                st.success("Done!")
+                st.write(f'There is no oil spill')
+                info.empty()
+                progress_bar.empty()
+                st.image(orig, channels='RGB', caption="TEST")
 
