36b0c17f3e4f78278c55c88a721454a38f41a209
Skibin Maksim <ski6a_23@mail.ru>
2023-04-06 16:24:09 +0300

    api: init infrastracture as dummy segmentation model

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 
+import numpy as np
+
 from infrastructure.segmentation import DummySegmentation
 
 
 if __name__ == '__main__':
     segmentation_model = DummySegmentation('path/to/weights')
-
+    test_img = np.random.random((100, 100, 1))
+    res = segmentation_model.segment(test_img)
+    print(res.shape)
 
